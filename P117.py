4ec53bb57199df3dd9b052479778adb533e25af4
AkristBharti <34163759+AkristBharti@users.noreply.github.com>
2021-03-18 15:40:10 +0530

    Update P117.py

--- P117.py
+++ P117.py
@@ -4,7 +4,6 @@ import plotly.graph_objects as gp
 import csv
 import plotly.figure_factory as pf
 import random
-
 import statistics
 import numpy as np
 import matplotlib.pyplot as plt
@@ -21,41 +20,41 @@ print(scores)
 
 results = df["class"]
 
-score_train, score_test, results_train, results_test = tts(scores, results, test_size= 0.25, random_state = 0)
+score_train, score_test, results_train, results_test = tts(scores, results, test_size= 0.25, random_state = 6)
 
 
 
 
-from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import StandardScaler
 
-ss_x = StandardScaler()
-score_train = ss_x.fit_transform(score_train)
-score_testt = ss_x.transform(score_test)
-
-
+#ss_x = StandardScaler()
+#score_train = ss_x.fit_transform(score_train)
+#score_testt = ss_x.transform(score_test)
 model = LogisticRegression(random_state = 0)
 model.fit(score_train, results_train)
-
-
 pred = model.predict(score_test)
 predict_value_1 = []
-
 for i in pred:
     if i == 0:
-        predict_value_1.append("Autherized")
+        predict_value_1.append("Authorized")
     else:
         predict_value_1.append("Forged")
-
 actual_value_1 = []
-
 for i in results_test.ravel():
     if i == 0:
-        actual_value_1.append("No")
+        actual_value_1.append("Authorized")
     else:
-         actual_value_1.append("Yes")
-
-            
+         actual_value_1.append("Forged")
 labels = ["Authorized", "Forged"]
 cm = confusion_matrix(actual_value_1, predict_value_1, labels)
 ax = plt.subplot()
 sb.heatmap(cm, annot = True, ax = ax)
+
+tn, fp, fn, tp = confusion_matrix(results_test, pred).ravel()
+print("True Negatives: ",tn)
+print("True Positives: ",tp)
+print("False Positives: ",fp)
+print("False Negatives: ",fn)
+
+Accuracy = (tn+tp)*100/(tp+tn+fp+fn)
+print("Accuracy :", (Accuracy))
